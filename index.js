aee814a2d8db6652e9e3457c2060fb29741480fb
Avishkanimsara <avishkaherath213@gmail.com>
2026-07-26 17:32:50 +0530

    Add bot files

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+ "name": :whatsapp-bot",
+ "version": "1.0.1",
+ "main": "index.js",
+ "scrtips": {
+   "start": "node index.js"
+  },
+  "dependencies": {
+   "@whiskeysockets/baileys": "latest",
+   "pino": "^8.14.0"
+  }
+}
+ 
